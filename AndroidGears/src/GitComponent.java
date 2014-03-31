4faf792b5328bf99bafcfdae4a53c1c199c30c05
Matt York <myork@cs.ua.edu>
2014-03-31 17:01:31 -0500

    - Began clone/pull task building
    - Added OSValidator

--- AndroidGears/src/GitComponent.java
+++ AndroidGears/src/GitComponent.java
@@ -1,15 +1,48 @@
 import com.intellij.openapi.components.ApplicationComponent;
+import org.eclipse.jgit.api.CloneCommand;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.PullCommand;
+import org.eclipse.jgit.api.errors.GitAPIException;
 import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by matthewyork on 3/31/14.
  */
 public class GitComponent implements ApplicationComponent {
+
+    private String REMOTE_SPECS_URL = "";
+
     public GitComponent() {
     }
 
     public void initComponent() {
-        // TODO: insert component initialization logic here
+
+        //Setup file
+        File androidGearsDirectory = null;
+
+        //Setup file
+        if (OSValidator.isWindows()) {
+            androidGearsDirectory = new File("C:\\AndroidGears"); //C drive
+        } else if (OSValidator.isMac()) {
+            androidGearsDirectory = new File("~/AndroidGears"); //Home folder
+        } else if (OSValidator.isUnix()) {
+            androidGearsDirectory = new File("~/AndroidGears"); //Home folder
+        } else if (OSValidator.isSolaris()) {
+            androidGearsDirectory = new File("~/AndroidGears");//Home folder
+        } else {
+            androidGearsDirectory = new File("~/AndroidGears");//Home folder
+        }
+
+        //Pull changes or clone repo
+        if(androidGearsDirectory.exists()){
+            pullChanges(androidGearsDirectory);
+        }
+        else {
+            cloneRepository(androidGearsDirectory);
+        }
     }
 
     public void disposeComponent() {
@@ -19,5 +52,31 @@ public class GitComponent implements ApplicationComponent {
     @NotNull
     public String getComponentName() {
         return "GitComponent";
+    }
+
+
+    //Git methods
+
+    private void cloneRepository(File androidGearsDirectory){
+        try {
+            Git.cloneRepository()
+                    .setURI(REMOTE_SPECS_URL)
+                    .setBranch("master")
+                    .setDirectory(androidGearsDirectory)
+                    .call();
+        } catch (GitAPIException e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    private void pullChanges(File androidGearsDirectory){
+        try {
+            Git git = Git.open(new File(androidGearsDirectory.getAbsolutePath()+".git"));
+            git.pull();
+        }
+        catch (IOException exception){
+            cloneRepository(androidGearsDirectory);
+        }
     }
 }
