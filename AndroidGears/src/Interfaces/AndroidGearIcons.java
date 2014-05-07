82821713b1be3b425e71dad9df4377fa2034d225
Aaron Fleshner <adfleshner@gmail.com>
2014-05-07 11:02:56 -0500

    Added jar Icon
    Added if jar icon to list render.
    therefore jar icon shows if it is a jar :)

--- AndroidGears/src/Interfaces/AndroidGearIcons.java
+++ AndroidGears/src/Interfaces/AndroidGearIcons.java
@@ -11,4 +11,5 @@ public interface AndroidGearIcons {
     public Icon ANDROID_GEAR_ICON = IconLoader.getIcon("/gears.png");
     public Icon declaredIcon =IconLoader.getIcon("/GearStateDeclared.png");//Fix
     public Icon installedIcon =IconLoader.getIcon("/GearStateInstalled.png");//Fix
+    public Icon jarfile =IconLoader.getIcon("/jarfile.png");//Fix
 }
